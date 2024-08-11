8464ac7f1d46a7ef8763ff6a059a8749e9fa2e40
SewoongPark <lympsw12@naver.com>
2024-08-11 18:43:49 +0900

    json 못 찾는 에러 추가

--- src/main/java/com/example/mssaembackendv2/global/common/CommonService.java
+++ src/main/java/com/example/mssaembackendv2/global/common/CommonService.java
@@ -1,6 +1,8 @@
 package com.example.mssaembackendv2.global.common;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import lombok.RequiredArgsConstructor;
@@ -15,8 +17,9 @@ public class CommonService {
         // ClassPathResource를 이용해 JSON 파일 읽어오기
         ClassPathResource resource = new ClassPathResource("community-terms.json");
 
-        // 파일 내용을 String으로 변환
-        Path path = resource.getFile().toPath();
-        return new String(Files.readAllBytes(path));
+        // InputStream을 이용해 파일 내용을 String으로 변환
+        try (InputStream inputStream = resource.getInputStream()) {
+            return new String(inputStream.readAllBytes(), StandardCharsets.UTF_8);
+        }
     }
 }
